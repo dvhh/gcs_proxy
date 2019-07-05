e179e246e7838c51bac92e84e1b10077267f6adb
dvhh <dvhh@yahoo.com>
2019-07-05 23:03:53 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Google Cloud Storage Bucket Proxy
+
+A proxy server to serve google cloud storage object to the web.
+
+# Requirements
+
+- Python3
+- Flask
+- Requests
+
+# Using
+
+Run from a machine having a service account, and ensure that the used service account have sufficient permission to read the bucket and the object you intent to serve via this proxy.
+
+Export to the environment
+- GCS_PROXY_STREAMING - set to 1 to enable response steam (disabled by default)
+- GCS_BUCKET - set to the bucket name the proxy should serve
+
+
+```
+$ export GCS_BUCKET=${BUCKET_NAME} 
+$ python3 gcs_proxy.py
+```
+
+You can invoke through any other wsgi server ( like gunicorn ).
+
+It is also recommended that you run this through a reverve application proxy, which would take care of extra details (such as caching
---
+++ gcs_proxy.py
@@ -0,0 +1,118 @@
+'''
+GCS Bucket Proxy
+
+Web proxy to serve GCS using service account token
+'''
+import time
+import urllib.parse
+import os
+import sys
+import requests
+from flask import Flask, Response, abort
+
+_bearer_token = None
+_bearer_token_ctime = 0
+
+_session = None
+
+
+def get_session():
+    '''
+    Singular requests session object for the current process
+    '''
+    global _session
+    if _session is None:
+        _session = requests.Session()
+    return _session
+
+
+def refresh_token():
+    '''
+    Refresh service account token
+    '''
+    session = get_session()
+    response = session.get(
+        'http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token',
+        headers={
+            'Metadata-Flavor': 'Google',
+            'User-Agent': 'GCS Proxy'
+        }
+    )
+    return response.json()
+
+
+def get_bearer_token():
+    '''
+    Ensure token freshness
+    '''
+    global _bearer_token
+    global _bearer_token_ctime
+    if time.time() - _bearer_token_ctime > 60:
+        _bearer_token = None
+
+    if _bearer_token is None:
+        _bearer_token = refresh_token()
+        _bearer_token_ctime = time.time()
+
+    return _bearer_token['access_token']
+
+
+app = Flask(__name__)
+
+
+@app.route('/<path:path>', methods=['GET'])
+def bucket_proxy(path: str):
+    '''
+    Object proxy handler
+    '''
+    token = get_bearer_token()
+    session = get_session()
+    path = urllib.parse.quote(path, safe='')
+    uri = 'https://www.googleapis.com/storage/v1/b/{}/o/{}?alt=media'.format(os.environ['GCS_BUCKET'], path)
+    streaming = int(os.environ.get('GCS_PROXY_STREAMING')) > 1
+    # sys.stderr.write('[{}]\n'.format(uri))
+    if streaming:
+        # streaming response
+        response = session.get(uri, headers={
+            'Authorization': 'Bearer {}'.format(token),
+            'User-Agent': 'GCS Proxy'
+        }, stream=True)
+        if response.status_code != 200:
+            response.close()
+            abort(response.status_code)
+
+        def send_response():
+            for chunk in response.iter_content(chunk_size=4096):
+                yield chunk
+            response.close()
+        result = Response(
+            send_response(),
+            headers={'Content-Length': response.headers['Content-Length']},
+            mimetype=response.headers['Content-Type']
+        )
+        return result
+    
+    # non streaming response
+    response = session.get(uri, headers={
+        'Authorization': 'Bearer {}'.format(token),
+        'User-Agent': 'GCS Proxy'
+    })
+    if response.status_code != 200:
+        response.close()
+        abort(response.status_code)
+    return response.content
+
+
+@app.route('/', methods=['GET'])
+def default_route():
+    '''
+    Default route
+    TODO: Do something more useful / relevant
+    '''
+    return 'OK'
+
+
+if __name__ == "__main__":
+    assert os.environ.get('GCS_BUCKET')
+    sys.stderr.write('{}\n'.format(os.environ.get('GCS_BUCKET')))
+    app.run(host='0.0.0.0', port=5000)
